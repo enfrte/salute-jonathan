9c9898521ecd79b981180a3a1767bb1d30c3bccb
enfrte <leon.forte.fi@gmail.com>
2025-01-19 13:30:11 +0200

    Added dynamic next chapter link

--- reader.php
+++ reader.php
@@ -1,8 +1,9 @@
 <?php 
 
-$lang = '';
-
-if ( !empty($_GET['lang']) ) {
+if ( !empty($_GET['lang']) && !empty($_GET['chapter']) ) {
+	displayTranslation($_GET['lang'], $_GET['chapter']);
+}
+else if ( !empty($_GET['lang']) ) {
 	displayTranslation($_GET['lang']);
 }
 
@@ -12,7 +13,14 @@ function displayTranslation(string $lang, int $chapter = 1) {
 	}
 
 	$url = __DIR__.'/translations/' . $lang . "/sj_" . $lang . "_" . $chapter. ".txt";
-	$fileContents = file_get_contents($url);
+
+	if (file_exists($url)) {
+		$fileContents = file_get_contents($url);
+	} else {
+		echo "File not found: " . $url;
+		return;
+	}
+
 	$result = '';
 	$lines = array_values(array_filter(array_map('trim', explode("\n", $fileContents)), 'strlen'));
 	
@@ -30,6 +38,20 @@ function displayTranslation(string $lang, int $chapter = 1) {
 			// Generate the HTML
 			$result .= '<tr><td>'.$lines[$i + 1].'</td><td>'. $lines[$i].'</td></tr>';
 		}
+	}
+
+	// Add a link to the next chapter
+
+	$chapter++;
+
+	if ($chapter < 10) {
+		$chapter = '0' . $chapter;
+	}
+
+	$nextChapterUrl = __DIR__.'/translations/' . $lang . "/sj_" . $lang . "_" . $chapter. ".txt";
+
+	if (file_exists($nextChapterUrl)) {
+		$result .= '<tr><td colspan="2"><a href="reader.php?lang='.$lang.'&chapter='.$chapter.'"><h3 class="mt-2">Next Chapter</h3></a></td></tr>';
 	}
 
 	echo $result;
